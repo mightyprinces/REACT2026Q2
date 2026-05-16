72834d948c2eb7fa39b0e3579eea40a267abc1e3
mightyprinces <happyrabbit62@gmail.com>
2026-05-17 01:24:46 +0300

    refactor: refactor class components to functional components: Results and Search

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,4 +1,3 @@
-import { Component, type ReactNode } from "react";
 import CardList from "./CardList";
 import type { CardType } from "../types";
 
@@ -8,31 +7,27 @@ type Props = {
   error: string;
 }
 
-export default class Results extends Component<Props> {
-  render(): ReactNode {
-    const {items, isLoading, error} = this.props;
-
-    if (isLoading) {
-      return (
-        <section className="results">
-          <span className="loader" role="status" aria-label="Loading"></span>
-          <p>Loading...</p>
-        </section>
-      )
-    }
-
-    if (error) {
-      return (
-        <section className="results">
-          <p className="errorMessage">{error}</p>
-        </section>
-      )
-    }
-
+export default function Results({ items, isLoading, error }: Props) {
+  if (isLoading) {
     return (
       <section className="results">
-        <CardList items={items}></CardList>
+        <span className="loader" role="status" aria-label="Loading"></span>
+        <p>Loading...</p>
       </section>
     )
   }
+
+  if (error) {
+    return (
+      <section className="results">
+        <p className="errorMessage">{error}</p>
+      </section>
+    )
+  }
+
+  return (
+    <section className="results">
+      <CardList items={items}></CardList>
+    </section>
+  )
 }
